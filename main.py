2c910e05737e80872147b2a4468668e309200606
Ahmed Sami <a.samii1250@gmail.com>
2023-03-23 22:39:25 +0200

    the app choose heat map value option

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
+import os
 import streamlit as st
 import leafmap.foliumap as leafmap
 import geopandas as gpd
 from handlers import * 
 
 
+
+
+
+
+
 m = leafmap.Map(tiles="stamentoner")
 
+def addAttributesOptions(gfile):
+    atts = gfile.columns
+    tuAtts = tuple(" ")+tuple(atts)
+    attList = st.selectbox("choose the value column",tuAtts)
+    if not attList == " ":
+        lat = gfile.geometry.y
+        long = gfile.geometry.x
+        value = gfile[attList]
+        st.write(value)
+        length = len(lat)
+        data = []
+        for i in range(0,length):
+            point = [lat[i],long[i],float(value[i])]
+            data.append(point)
+        st.write(data)
 
+        m.add_heatmap(data)
+        
 
 with st.sidebar:
     st.markdown("<h1 style='color: aqua; text-align: center;'>AS MAP</h1>",unsafe_allow_html=True)
-    baseList = st.selectbox("Choose your Basemap",('Open Street Map','Google HYBRID','Esri NatGeoWorld'))
+    baseList = st.selectbox("Choose your Basemap",('Open Street Map',"stamentoner",'Google HYBRID','Esri NatGeoWorld'))
     if baseList == "Open Street Map":
         pass         
+    elif baseList == "stamentoner":
+        st.write("iam here")
+        m.add_basemap(tiles="stamentoner")
     elif  baseList == "Google HYBRID":
-         m.add_basemap("HYBRID")
-        # m.add_tile_layer(
-        #     url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-        #     name="Google Satellite",
-        #     attribution="Google",
-        #     )
+        m.add_basemap("HYBRID")
     elif baseList == 'Esri NatGeoWorld':
         m.add_basemap("Esri.NatGeoWorldMap")
     heatFile = st.file_uploader("upload file for heat map",type="geojson")
     if heatFile:
-        # filepath = "./reprojected.geojson"
-        gfile = gpd.read_file(heatFile)
-        st.write(gfile.crs)
-        st.write(gfile.crs==3857)
-        if not gfile.crs == 3857:
-            gfile = gfile.to_crs(3857)
+        gfile = gpd.read_file(heatFile).set_crs(3857,allow_override=True)
+        if  gfile.crs == 3857:
+            gfile = gfile.to_crs(4326)
+            st.write(gfile.geometry)
+        addAttributesOptions(gfile)
         m.add_gdf(gfile)
-    #     m.add_heatmap(
-    #     filepath,
-    #     latitude="latitude",
-    #     longitude="longitude",
-    #     value="pop_max",
-    #     name="Heat map",
-    #     radius=20,
-    # )
-
-
 
 
 m.to_streamlit(width=1000,height=700)
+
+
+    
+
 
 
 
